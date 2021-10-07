6c8b71080b2b65b78fb4e01fab0c46cae593c805
TheMining72 <60348632+TheMining72@users.noreply.github.com>
2021-10-07 21:35:30 +0630

    Update test file

--- test/index.js
+++ test/index.js
@@ -1,20 +1,25 @@
 const { MisaMino } = require("../src/index.js");
 
-const c = {
+// 0 = Max Depth
+const PPS = 30;
+
+// Piece colors for printField()
+const PIECE_COLORS = {
   Reset: "\x1b[0m",
 
-  BgBlack: "\x1b[40m",
-  BgRed: "\x1b[41m",
-  BgGreen: "\x1b[42m",
-  BgYellow: "\x1b[48;5;220m",
-  BgOrange: "\x1b[48;5;208m",
-  BgBlue: "\x1b[44m",
-  BgMagenta: "\x1b[45m",
-  BgCyan: "\x1b[46m",
-  BgWhite: "\x1b[47m"
+  " ": "\x1b[47m  ",
+  "G": "\x1b[48;5;239m  ",
+  "Z": "\x1b[41m  ",
+  "S": "\x1b[42m  ",
+  "L": "\x1b[48;5;208m  ",
+  "J": "\x1b[44m  ",
+  "T": "\x1b[45m  ",
+  "O": "\x1b[48;5;220m  ",
+  "I": "\x1b[46m  "
 }
 
-class piecesGen {
+// Piece generator
+class PiecesGen {
   constructor(seed) {
     this.startSeed = seed % 2147483647;
     this.curSeed = seed % 2147483647;
@@ -43,8 +48,9 @@ class piecesGen {
   }
 }
 
-var queueGen = new piecesGen(1);
-var queue = []; queueGen.nextBag().forEach(p => queue.push(p)); queueGen.nextBag().forEach(p => queue.push(p));
+// Game data
+var queueGen = new PiecesGen(1);
+var queue = []; queue.push(...queueGen.nextBag()); queue.push(...queueGen.nextBag()); queue.push(...queueGen.nextBag()); queue.push(...queueGen.nextBag()); queue.push(...queueGen.nextBag());
 var hold = "T";
 var current = "I";
 var combo = 0;
@@ -52,61 +58,49 @@ var b2b = 0;
 var field = [];
 for (var x = 0; x < 10; x++) {
   field.push([]);
-  for (var y = 0; y < 20; y++)
-    field[x].push(-1);
+  for (var y = 0; y < 22; y++)
+    field[x].push(" ");
 }
+field = MisaMino.add_garbage(field, [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 1, 2]);
 
+// Information
+var atk = 0;
+var piecesPlaced = 0;
+var startTime = Date.now();
+var depth = 0;
+
+// Prints the field
 function printField() {
   console.clear();
   let log = [];
 
   for (let y = field[0].length - 1; y >= 0; y--) {
     log.push([]);
-    for (let x = field.length - 1; x >= 0; x--){
-      switch (field[x][y]) {
-        case -1:
-          log[field[0].length - y - 1].push(c.BgWhite + '  ');
-          break;
-        case 0:
-          log[field[0].length - y - 1].push(c.BgRed + '  ');
-          break;
-        case 1:
-          log[field[0].length - y - 1].push(c.BgGreen + '  ');
-          break;
-        case 2:
-          log[field[0].length - y - 1].push(c.BgOrange + '  ');
-          break;
-        case 3:
-          log[field[0].length - y - 1].push(c.BgBlue + '  ');
-          break;
-        case 4:
-          log[field[0].length - y - 1].push(c.BgMagenta + '  ');
-          break;
-        case 5:
-          log[field[0].length - y - 1].push(c.BgYellow + '  ');
-          break;
-        case 6:
-          log[field[0].length - y - 1].push(c.BgCyan + '  ');
-          break;
-      }
-    }
-    log[field[0].length - y - 1].push("\n" + c.Reset);
+    for (let x = field.length - 1; x >= 0; x--)
+      log[field[0].length - y - 1].push(PIECE_COLORS[field[x][y]]);
+    log[field[0].length - y - 1].push("\n" + PIECE_COLORS.Reset);
     log[field[0].length - y - 1] = log[field[0].length - y - 1].join("");
   }
   console.log(log.join(""));
-  console.log(`[${hold}] ${current} ${queue.join("")}`);
-  console.log(`B2B: ${b2b} Combo: ${combo}`);
-}
 
-MisaMino.configure();
-var piecesLeft = 14;
+  console.log(`[${hold}] ${current} ${queue.join("")} >`);
+  console.log(`Depth: ${depth}`);
+  console.log(`Combo: ${combo} B2B: ${b2b}`);
+  var curSec = (Date.now() - startTime) / 1000;
+  console.log(`PPS: ${(piecesPlaced / curSec).toFixed(2)} APP: ${(atk / piecesPlaced).toFixed(2)} APM: ${(atk / (curSec / 60)).toFixed(2)} APS: ${(atk / curSec).toFixed(2)}`);
+}
+printField();
+
+// Configure
+MisaMino.configure({avoid_softdrop: 0});
+let nextBag = queueGen.nextBag();
 
 async function play() {
-  piecesLeft--;
-  if (piecesLeft < 7) {
-    queueGen.nextBag().forEach(p => queue.push(p));
-    piecesLeft += 7;
-  }
+  // Queue
+  nextBag.length <= 1 && nextBag.push(...queueGen.nextBag());
+  queue.push(...nextBag.splice(0, 1));
+
+  // Give game data to MisaMinoJS
   MisaMino.update_next(queue);
   MisaMino.update_hold(hold);
   MisaMino.update_current(current);
@@ -115,31 +109,38 @@ async function play() {
   MisaMino.update_b2b(b2b);
   MisaMino.update_field(field);
 
-  let s = MisaMino.action();
-  // 0 = Search everything without taking the pps in
-  let pps = 4;
+  // Start calculating
+  let actionPromise = MisaMino.action();
   let actionTimeout;
-  if (pps > 0) actionTimeout = setTimeout(MisaMino.abort, Math.floor(1000 / pps))
-  s.then(solution => {
-    if (pps > 0) clearTimeout(actionTimeout);
+  if (PPS > 0) actionTimeout = setTimeout(MisaMino.abort, Math.floor(1000 / PPS))
+
+  actionPromise.then(solution => {
+    clearTimeout(actionTimeout);
+
+    // Field
+    newField = MisaMino.apply_piece(field, solution);
+    field = newField.Field;
+
+    // Game data
+    piecesPlaced++;
+    b2b = solution.B2B;
+    atk += solution.Attack;
+    depth = solution.Depth;
+    if(newField.Combo) combo++;
+    else combo = 0;
+
+    // Queue & Hold
     if (solution.Instructions.includes(10))
       hold = current;
     current = queue[0];
     queue.shift();
-    new_field = MisaMino.apply_piece(field, solution);
-    if (!new_field.Success) {
-      console.log("Failed to apply piece")
-      return;
-    };
-    field = new_field.Field;
-    if(new_field.Combo) combo++;
-    else combo = 0;
-    b2b = solution.B2B;
 
+    // Continue if not dead
     printField();
+    if (!MisaMino.alive() || !newField.Success) return;
     play();
   });
 };
 
-console.clear();
+// Start
 play();
